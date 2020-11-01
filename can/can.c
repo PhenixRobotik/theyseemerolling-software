3a453ffd970cc69363c8785a835a2f9adbdfe0ad
42Max42 <maxime.nabon@gmail.com>
2020-11-01 17:36:54 +0100

    parenthesis for safety

--- can/can.c
+++ can/can.c
@@ -59,8 +59,8 @@ int can_setup(void){
   /* CAN filter 0 init */
   can_filter_id_mask_32bit_init(
     0,     /* Filter ID */
-    CAN_ID_TSMR<<19 | CAN_ID_RPI<<3,     /* CAN ID */
-    0x0F<<19 | 0xFF<<3,     /* CAN ID mask */
+    (CAN_ID_TSMR<<19) | (CAN_ID_RPI<<3),     /* CAN ID */
+    (0x0F<<19) | (0xFF<<3),     /* CAN ID mask */
     0,     /* FIFO assignment (here: FIFO0) */
     true); /* Enable the filter. */
 
